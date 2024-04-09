437c4644d754a6abf91af24dd119320fdc70944a
Bucky TheWinterSoldier <nagendran.ashok@outlook.com>
2024-04-09 15:10:55 +0000

    Exercise Tracker

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
+const URL=require('url');
+const mongoose=require('mongoose');
 require('dotenv').config()
 
 app.use(cors())
@@ -8,8 +11,22 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+app.use(bodyParser.urlencoded({ extended: false }))
+mongoose.connect('mongodb+srv://ashokravi:ashokravi@cluster0.xk9uy7l.mongodb.net/ExerciseTracker?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{console.log('Connected!')});
+const userSchema=mongoose.Schema({
+  userName:{
+    type:String,
+    required:true
+  }
+});
+const userModel=mongoose.model("userModel",userSchema);
 
-
+app.post('/api/users',async(request,response)=>{
+const username= request.body.username;
+const responsePayload=await userModel.create({userName:username});
+response.json({username:username,id:responsePayload._id});
+})
 
 
 
